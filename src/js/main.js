5207ffd733f12b62bd292141e656985d57d82875
Jigga Byte <jigga.e10@gmail.com>
2025-02-13 16:00:41 +0100

    added week 2 final assignments and alert.js feature

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import Alert from "./Alert.mjs";
 // import ProductData from "./ProductData.mjs";
 // import ProductListing from "./ProductList.mjs";
 
-
 //const productData = new ProductData();
 
 document.addEventListener("DOMContentLoaded", () => {
